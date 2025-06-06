979ca0b9816f53fc6b44af46cc0fdce6d0c55cca
thuvaragan-dm <thuvaragan@deepmodel.ai>
2025-06-06 12:08:35 +0530

    api url changed

--- src/api/variables.ts
+++ src/api/variables.ts
@@ -1,2 +1,3 @@
-export const apiUrl = "https://dm-api-dot-dm-development-001.uc.r.appspot.com";
-//export const apiUrl = "https://dm-api-cloud-run-sandbox-238037281416.us-central1.run.app";
+// export const apiUrl = "https://dm-api-dot-dm-development-001.uc.r.appspot.com";
+export const apiUrl =
+  "https://dm-api-cloud-run-sandbox-238037281416.us-central1.run.app";
