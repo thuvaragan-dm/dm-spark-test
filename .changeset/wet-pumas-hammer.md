73a162ff66ee7c4c586f6474d869952b9122d043
Thuvaragan Parameshwaran <thuvaraganuidesigns@gmail.com>
2025-06-10 22:14:16 +0530

    added explore agents.

---
+++ .changeset/wet-pumas-hammer.md
@@ -0,0 +1,5 @@
+---
+"deepmodel-app": patch
+---
+
+fixed minor bugs and added explore agents
--- src/api/variables.ts
+++ src/api/variables.ts
@@ -1,4 +1,5 @@
 // export const apiUrl = "https://dm-api-dot-dm-development-001.uc.r.appspot.com";
+
 export const apiUrl =
   "https://dm-api-cloud-run-sandbox-238037281416.us-central1.run.app";
 
